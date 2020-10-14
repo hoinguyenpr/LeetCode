b14698bd2186184f2783c6e8b18e6f6880b9c1ee
hoinguyenpr <hoinguyenpr.97@gmail.com>
2020-10-15 00:01:53 +0700

    1089 - Duplicate Zeros

---
+++ src/com/leetcode/array/duplicateZeros_v1.java
@@ -0,0 +1,32 @@
+package com.leetcode.array;
+
+public class duplicateZeros_v1 {
+	public static void main(String[] args) {
+		int a[] = {1,1,0};
+		duplicateZeros(a);
+		for(int i = 0; i < a.length; i++) {
+			System.out.println(a[i]);
+		}
+	}
+	
+	public static void insertArray(int[] array, int a, int index) {
+		for(int i = array.length - 1; i > index; i --) {
+			array[i] = array[i - 1];
+		}
+		array[index] = a;
+	}
+	
+	public static void duplicateZeros(int[] a) {
+		boolean flag = false;
+		for(int i = 0; i < a.length - 1; i ++) {
+			if(flag == true) {
+				flag = false;
+				continue;
+			}
+			if(a[i] == 0) {
+				flag = true;
+				insertArray(a, 0, i + 1);
+			}
+		}
+	}
+}
